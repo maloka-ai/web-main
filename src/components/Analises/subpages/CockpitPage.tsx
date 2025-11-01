687cea0b122d8d9ea1ece3817d79693b0896c298
Francisco Junior <rivailjunior0906@gmail.com>
2025-11-01 15:20:22 -0400

    refactor: Alterações de responsividade nos layouts de análises

--- src/components/Analises/subpages/CockpitPage.tsx
+++ src/components/Analises/subpages/CockpitPage.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
 import InsightsOutlinedIcon from '@mui/icons-material/InsightsOutlined';
 import AlertasEAcoes from '../widgets/AlertasEAcoes';
 import { useEffect, useState } from 'react';
@@ -18,6 +18,7 @@ import {
 } from '../helpers/CockpitHelper';
 import ResumeGraph from '../widgets/ResumeGraph';
 import { GraphData } from '@/utils/graphics';
+import { useIsMobile } from '@/hooks/useIsMobile';
 
 export default function CockpitPage() {
   const [customer, setCustomer] = useState<CustomerSegmentation[]>([]);
@@ -34,7 +35,7 @@ export default function CockpitPage() {
   const [lastYearDailyRevenues, setLastYearDailyRevenues] = useState<any[]>([]);
   const [stock, setStock] = useState<any[]>([]);
   const [cockpitAlert, setCockpitAlert] = useState<CockpitAlert[]>([]);
-
+  const isMobile = useIsMobile();
   useEffect(() => {
     analysisService
       .getSegmentacaoClientes()
@@ -85,17 +86,57 @@ export default function CockpitPage() {
         display: 'flex',
         gap: '1.5rem',
         height: 'calc(100vh - 100px)',
-        overflow: 'hidden',
+        overflow: {
+          xs: 'visible',
+          md: 'hidden',
+        },
+        flexDirection: {
+          xs: 'column',
+          md: 'row',
+        },
       }}
     >
-      <Box sx={{ width: '350px', overflow: 'auto', pr: 1, pb: 16 }}>
+      <Box
+        sx={{
+          width: {
+            xs: '100%',
+            md: '350px',
+          },
+          overflow: 'auto',
+          pr: 1,
+          pb: 16,
+          minHeight: {
+            xs: '50%',
+            md: 'initial',
+          },
+          backgroundColor: {
+            xs: '#f9f7f1',
+            md: 'transparent',
+          },
+          borderRadius: {
+            xs: '12px',
+            md: '0px',
+          },
+        }}
+      >
         <Typography variant="subtitle1" fontWeight={600} color="#4b4b4b" mb={1}>
           Alertas e Ações
         </Typography>
+        {isMobile && <Divider />}
         <AlertasEAcoes cockpitAlert={cockpitAlert} />
       </Box>
 
-      <Box sx={{ flex: 1, overflowY: 'auto', pr: 1, pb: 10 }}>
+      <Box
+        sx={{
+          flex: 1,
+          overflowY: {
+            xs: 'visible',
+            md: 'auto',
+          },
+          pr: 1,
+          pb: 10,
+        }}
+      >
         <Typography variant="h6" fontWeight={600} color="#4b4b4b" mb={1.5}>
           Resultados • KPIs
         </Typography>
@@ -144,7 +185,11 @@ export default function CockpitPage() {
                 padding: '4px',
                 display: 'grid',
                 gap: '1rem',
-                gridTemplateColumns: 'repeat(3, minmax(250px, 1fr))',
+                gridTemplateColumns: {
+                  xs: 'repeat(1, minmax(250px, 1fr))',
+                  sm: 'repeat(2, minmax(250px, 1fr))',
+                  md: 'repeat(3, minmax(250px, 1fr))',
+                },
               }}
             >
               {annualRevenues &&
@@ -199,7 +244,11 @@ export default function CockpitPage() {
                 display: 'grid',
                 mb: 4,
                 gap: '1rem',
-                gridTemplateColumns: 'repeat(3, minmax(250px, 1fr))',
+                gridTemplateColumns: {
+                  xs: 'repeat(1, minmax(250px, 1fr))',
+                  sm: 'repeat(2, minmax(250px, 1fr))',
+                  md: 'repeat(3, minmax(250px, 1fr))',
+                },
               }}
             >
               {stock &&
@@ -246,7 +295,11 @@ export default function CockpitPage() {
                 padding: '4px',
                 display: 'grid',
                 gap: '1rem',
-                gridTemplateColumns: 'repeat(3, minmax(250px, 1fr))',
+                gridTemplateColumns: {
+                  xs: 'repeat(1, minmax(250px, 1fr))',
+                  sm: 'repeat(2, minmax(250px, 1fr))',
+                  md: 'repeat(3, minmax(250px, 1fr))',
+                },
               }}
             >
               {customer &&
